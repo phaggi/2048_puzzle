a22644e0d50d9615f08f7b3901ae135c6dc779ed
phaggi <phaggi@gmail.com>
2021-01-02 13:31:31 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class Matrix:
+    def __init__(self):
+        pass
+
+def runme():
+    # Use a breakpoint in the code line below to debug your script.
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    runme()
